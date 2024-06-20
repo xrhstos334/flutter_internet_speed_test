28852df2791af02bf2bbd539478fbaf62e1413a4
xrhstos334 <74309761+xrhstos334@users.noreply.github.com>
2024-06-20 10:21:29 +0300

    downgrade

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -1,6 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 Future<bool> isInternetAvailable() async {
-  List<ConnectivityResult> connectivityResult = await (Connectivity().checkConnectivity());
-  return connectivityResult.first == ConnectivityResult.mobile || connectivityResult.first == ConnectivityResult.wifi;
+  ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
+  return connectivityResult == ConnectivityResult.mobile || connectivityResult == ConnectivityResult.wifi;
 }
