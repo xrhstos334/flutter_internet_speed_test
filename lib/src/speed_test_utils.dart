a82b927719861c901e3283bd8b00187d43970117
xrhstos334 <74309761+xrhstos334@users.noreply.github.com>
2025-09-03 14:00:41 +0300

    update packages

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -4,6 +4,6 @@ Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
   final connectivityResult = await connectivity.checkConnectivity();
   return connectivityResult.first == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+      connectivityResult.first == ConnectivityResult.wifi ||
+      connectivityResult.first == ConnectivityResult.ethernet;
 }
